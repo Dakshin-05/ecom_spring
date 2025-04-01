34c4b2be356da914904acaa576826034df7a5e1f
Dakshin-05 <mrdm1869@gmail.com>
2025-04-01 08:18:02 +0530

    Swagger UI completed

--- src/main/java/com/ecom/customerorder/controller/OrderController.java
+++ src/main/java/com/ecom/customerorder/controller/OrderController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 
-@Tag(name = "Customer API", description = "Operations related to customers")
+@Tag(name = "Orders API", description = "Operations related to Orders")
 @RequestMapping("/orders")
 public class OrderController {
     @Autowired
